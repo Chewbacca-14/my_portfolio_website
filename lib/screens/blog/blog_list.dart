3ed3975f32087ff0a8ca35d5a2eb02ae0911fd91
Max <bulanovich5@seznam.cz>
2025-10-01 22:58:53 +0200

    added new blog

--- lib/screens/blog/blog_list.dart
+++ lib/screens/blog/blog_list.dart
@@ -2,23 +2,24 @@ part of 'blog.dart';
 
 List<Blog> blog = [
   Blog(
-    networkImageUrl:
-        'https://miro.medium.com/v2/resize:fit:720/format:webp/0*EvjAPiItnXy15KDR.png',
+    networkImageUrl: 'https://miro.medium.com/v2/resize:fit:720/format:webp/0*EvjAPiItnXy15KDR.png',
     title: 'Must-Know Flutter Packages in 2025 1. part',
-    description:
-        'In this article, I will go over the packages that everyone developing Flutter apps in 2025 should know.',
-    url:
-        'https://medium.com/@bulanovich5/must-know-flutter-packages-in-2025-1-part-4499a2f8b949',
+    description: 'In this article, I will go over the packages that everyone developing Flutter apps in 2025 should know.',
+    url: 'https://medium.com/@bulanovich5/must-know-flutter-packages-in-2025-1-part-4499a2f8b949',
     date: DateTime(2025, 2, 23),
   ),
   Blog(
-    networkImageUrl:
-        'https://miro.medium.com/v2/resize:fit:720/format:webp/1*stUx88F8Cy-pgDr6AP9njA.png',
+    networkImageUrl: 'https://miro.medium.com/v2/resize:fit:720/format:webp/1*stUx88F8Cy-pgDr6AP9njA.png',
     title: 'Must-Know Flutter Packages in 2025 2. part',
-    description:
-        'In the 2nd part, we dive into images, maps, dependency injection, and routing.',
-    url:
-        'https://medium.com/@bulanovich5/must-know-flutter-packages-in-2025-part-2-a6e340257192',
+    description: 'In the 2nd part, we dive into images, maps, dependency injection, and routing.',
+    url: 'https://medium.com/@bulanovich5/must-know-flutter-packages-in-2025-part-2-a6e340257192',
     date: DateTime(2025, 3, 25),
+  ),
+  Blog(
+    networkImageUrl: 'https://miro.medium.com/v2/resize:fit:4800/format:webp/0*Ri86Y7NF1uOteRUw.png',
+    title: 'Master Flutter APIs in Minutes: Build Your First App Today',
+    description: 'A simple tutorial to help you understand and use APIs in Flutter for building apps easily.',
+    url: 'https://medium.com/@bulanovich5/master-flutter-apis-in-minutes-build-your-first-app-today-9ed14aa9ff39',
+    date: DateTime(2025, 10, 1),
   ),
 ];
