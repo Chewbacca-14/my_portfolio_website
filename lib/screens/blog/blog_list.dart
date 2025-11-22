65093e9b44254851ff14eae229b341499bda6de8
Max <bulanovich5@seznam.cz>
2025-11-22 17:17:50 +0100

    added new blogs

--- lib/screens/blog/blog_list.dart
+++ lib/screens/blog/blog_list.dart
@@ -2,18 +2,25 @@ part of 'blog.dart';
 
 List<Blog> blog = [
   Blog(
-    networkImageUrl: 'https://miro.medium.com/v2/resize:fit:720/format:webp/0*EvjAPiItnXy15KDR.png',
-    title: 'Must-Know Flutter Packages in 2025 1. part',
-    description: 'In this article, I will go over the packages that everyone developing Flutter apps in 2025 should know.',
-    url: 'https://medium.com/@bulanovich5/must-know-flutter-packages-in-2025-1-part-4499a2f8b949',
-    date: DateTime(2025, 2, 23),
+    networkImageUrl: 'https://miro.medium.com/v2/resize:fit:1400/format:webp/1*LFP7ghpjz1FNbcRo4A9aIQ.png',
+    title: 'Dart & Flutter 2. Dart Basics — Easier Than You Think',
+    description: 'Part 2 of Dart & Flutter series: beginner-friendly Dart and Flutter guide.',
+    url: 'https://medium.com/@bulanovich5/dart-flutter-2-dart-basics-easier-than-you-think-93b9314a8cb6',
+    date: DateTime(2025, 11, 22),
   ),
   Blog(
-    networkImageUrl: 'https://miro.medium.com/v2/resize:fit:720/format:webp/1*stUx88F8Cy-pgDr6AP9njA.png',
-    title: 'Must-Know Flutter Packages in 2025 2. part',
-    description: 'In the 2nd part, we dive into images, maps, dependency injection, and routing.',
-    url: 'https://medium.com/@bulanovich5/must-know-flutter-packages-in-2025-part-2-a6e340257192',
-    date: DateTime(2025, 3, 25),
+    networkImageUrl: 'https://miro.medium.com/v2/resize:fit:1400/format:webp/1*KEJG8fthwuDM9b3rmmkcFw.png',
+    title: 'Dart & Flutter 1. Why Dart? Everything You Need Before Starting',
+    description: 'Part 1 of Dart & Flutter series: beginner-friendly Dart and Flutter guide.',
+    url: 'https://medium.com/@bulanovich5/dart-flutter-1-why-dart-everything-you-need-before-starting-f5b4f1383063',
+    date: DateTime(2025, 11, 22),
+  ),
+  Blog(
+    networkImageUrl: 'https://miro.medium.com/v2/resize:fit:1400/format:webp/1*cr_XfzaaeoC80DXrTcIC4Q.jpeg',
+    title: 'Highlights from FlutterCon 2025',
+    description: 'What I learned & how I’m applying it',
+    url: 'https://medium.com/@bulanovich5/highlights-from-fluttercon-2025-what-i-learned-how-im-applying-it-0928cfa0928c',
+    date: DateTime(2025, 11, 16),
   ),
   Blog(
     networkImageUrl: 'https://miro.medium.com/v2/resize:fit:1100/format:webp/0*Ri86Y7NF1uOteRUw',
@@ -23,11 +30,17 @@ List<Blog> blog = [
     date: DateTime(2025, 10, 1),
   ),
   Blog(
-    networkImageUrl: 'https://miro.medium.com/v2/resize:fit:1400/format:webp/1*cr_XfzaaeoC80DXrTcIC4Q.jpeg',
-    title: 'Highlights from FlutterCon 2025',
-    description: 'What I learned & how I’m applying it',
-    url: 'https://medium.com/@bulanovich5/highlights-from-fluttercon-2025-what-i-learned-how-im-applying-it-0928cfa0928c',
-    date: DateTime(2025, 11, 16),
+    networkImageUrl: 'https://miro.medium.com/v2/resize:fit:720/format:webp/1*stUx88F8Cy-pgDr6AP9njA.png',
+    title: 'Must-Know Flutter Packages in 2025 2. part',
+    description: 'In the 2nd part, we dive into images, maps, dependency injection, and routing.',
+    url: 'https://medium.com/@bulanovich5/must-know-flutter-packages-in-2025-part-2-a6e340257192',
+    date: DateTime(2025, 3, 25),
   ),
-  
+  Blog(
+    networkImageUrl: 'https://miro.medium.com/v2/resize:fit:720/format:webp/0*EvjAPiItnXy15KDR.png',
+    title: 'Must-Know Flutter Packages in 2025 1. part',
+    description: 'In this article, I will go over the packages that everyone developing Flutter apps in 2025 should know.',
+    url: 'https://medium.com/@bulanovich5/must-know-flutter-packages-in-2025-1-part-4499a2f8b949',
+    date: DateTime(2025, 2, 23),
+  ),
 ];
